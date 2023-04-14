2fe30de1c2c83434586f99d3e1044dd8340bce5a
donhee <saytokang@gmail.com>
2023-04-14 14:32:37 +0900

    kafka send() 결과 확인 기능 추가

--- src/main/java/com/example/demokafkahelloworld/jsonSer/UserService.java
+++ src/main/java/com/example/demokafkahelloworld/jsonSer/UserService.java
@@ -4,13 +4,21 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserService {
     private final KafkaTemplate<String, User> kafkaTemplate;
 
     public void createUser(User user) {
-        kafkaTemplate.send(KafkaConfigOnUser.TOPIC_NAME, user.getId()+"", user);
+        kafkaTemplate
+            .send(KafkaConfigOnUser.TOPIC_NAME, user.getId()+"", user)
+            .thenAccept(rs -> log.info(" send OK. offset: {}", rs.getRecordMetadata().offset()))
+            .exceptionally(ex -> {
+                log.error("{}", ex);
+                return null;
+            });
     }
 }
